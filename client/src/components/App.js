97e19d6ded43dda5894bd824fd2300eedbe729fd
bharath-s95 <36877669+bharath-s95@users.noreply.github.com>
2019-12-18 18:45:30 +0530

    header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
+
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
   return(
-    <div>App</div>
+    <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header/>
+            <Route path="/" component={Landing} exact={true}/>
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </ BrowserRouter>
+     </div>
   );
 };
 
