ac5b3f51d21b13cd941dda27f73428534806b907
bharath-s95 <36877669+bharath-s95@users.noreply.github.com>
2019-12-18 20:31:37 +0530

    header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,16 @@
 import React,{Component} from 'react';
+import {connect} from 'react-redux';
 
 class Header extends Component{
+  renderContent = () => {
+    switch(this.props.auth){
+      case null: return;
+      case false: return (
+        <li><a href="/auth/google">Login With Google</a></li>
+      );
+      default: return (<li><a>Logout</a></li>);
+    }
+  }
   render(){
     return(
       <nav>
@@ -9,9 +19,7 @@ class Header extends Component{
             Emaily
           </a>
           <ul className="right">
-            <li>
-              <a>Login with Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -19,4 +27,7 @@ class Header extends Component{
   };
 };
 
-export default Header;
+const mapStateToProps = ({auth}) => {
+  return {auth};
+}
+export default connect(mapStateToProps)(Header);
