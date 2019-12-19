b24fec8761e88b7f65a997e4e7781580c5cd13ec
bharath-s95 <36877669+bharath-s95@users.noreply.github.com>
2019-12-19 18:58:23 +0530

    Completed Client Side

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 
 class Header extends Component{
   renderContent = () => {
@@ -8,16 +9,19 @@ class Header extends Component{
       case false: return (
         <li><a href="/auth/google">Login With Google</a></li>
       );
-      default: return (<li><a>Logout</a></li>);
+      default: return (<li><a href="/api/logout">Logout</a></li>);
     }
   }
   render(){
     return(
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">
+          <Link
+            to={this.props.auth ? '/surveys' : '/'} 
+            className="left brand-logo"
+          >
             Emaily
-          </a>
+          </Link>
           <ul className="right">
             {this.renderContent()}
           </ul>
