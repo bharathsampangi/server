e9d3f4afaa74751c26df92406c23ee0b482b5344
bharath-s95 <36877669+bharath-s95@users.noreply.github.com>
2019-12-27 19:07:00 +0530

    Completed Survey Creation

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,16 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
 import {Provider} from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import reducers from './reducers';
 import reduxThunk from 'redux-thunk';
+
+import App from './components/App';
+import reducers from './reducers';
+
+//Development only axios helpers
+import axios from 'axios';
+window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
@@ -17,4 +22,4 @@ ReactDOM.render(
 );
 
 console.log('STRIPE KEY is', process.env.REACT_APP_STRIPE_KEY);
-console.log('Environment is',process.env.NODE_ENV); 
+console.log('Environment is',process.env.NODE_ENV);
